6af4cbe2b2f165164ed18169095ef60f48c20274
Shohei Otsuka <tsukacchaaaan@gmail.com>
2022-03-10 20:17:55 +0900

    Packages, variables, and functions.

---
+++ basics/exported-names.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	//fmt.Printf(math.pi)
+	fmt.Println(math.Pi)
+}
---
+++ basics/imports.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//import "fmt"
+//import "math"
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
